e985e541066f3e4a542e2a5cc3676c37c1bc277e
Tran Viet Dung <dungviettran89@gmail.com>
2025-10-07 01:04:05 +0800

    added support for qwen and gemini as the main coder

--- .roo/rules/rules.md
+++
@@ -1,2 +0,0 @@
-Always read agent.md first. 
-If the content of agent.md is not in your context, read again
--- gemini.sh
+++ gemini.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+ln -sf AGENTS.md GEMINI.md
+ln -sf .agentsignore .geminiignore
 npm i --prefix=.bin -g @google/gemini-cli
 if [ -f ".bin/gemini.cmd" ]; then
   .bin/gemini.cmd "$@"
